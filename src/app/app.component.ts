9627083588ee308be885e53aec175e0c1e1c42c7
Rémi Gueirard <remi.gueirard@altran.com>
2018-04-12 09:20:51 +0200

    download file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as domtoimage from 'dom-to-image';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'satt-avarre-front',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ID.vet';
+  image : any;
+  url : string;
+
+
+  printGrid(): void {
+    let node = document.getElementById('la-plate-grid');
+
+    domtoimage.toPng(node)
+      .then((dataUrl) => {
+        this.image = new Image();
+        this.image.src = dataUrl;
+        this.url = dataUrl;
+        document.body.appendChild(this.image);
+      })
+      .catch(function (error) {
+        console.error('oops, something went wrong!', error);
+      });
+
+    domtoimage.toBlob(node)
+      .then(function (blob) {
+        FileSaver.saveAs(blob, 'my-node.png');
+        // console.log('coucou');
+      });
+  }
 }
--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as domtoimage from 'dom-to-image';
+
 
 @Component({
   selector: 'grid-comp',
@@ -8,32 +8,11 @@ import * as domtoimage from 'dom-to-image';
 })
 export class GridComponent implements OnInit {
 
-  image : any;
-  url : string;
-
   constructor() { }
 
   ngOnInit() {
   }
 
-  printGrid(): void {
-    let node = document.getElementById('plate-grid');
 
-    domtoimage.toPng(node)
-      .then((dataUrl) => {
-        this.image = new Image();
-        this.image.src = dataUrl;
-        this.url = dataUrl;
-        // document.body.appendChild(this.image);
-      })
-      .catch(function (error) {
-        console.error('oops, something went wrong!', error);
-      });
-
-    // domtoimage.toBlob(node)
-    //   .then(function (blob) {
-    //     window.saveAs(blob, 'my-node.png');
-    //   });
-  }
 
 }
